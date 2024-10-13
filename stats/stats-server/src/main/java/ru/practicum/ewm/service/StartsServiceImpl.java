aca4b696eb5c672653c97a0c82d09c4d03fe2ddd
Ilya <fybiby.2@yandex.ru>
2024-10-13 17:57:57 +0300

    Fix problem with checkstyle

--- stats/stats-server/src/main/java/ru/practicum/ewm/service/StartsServiceImpl.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/service/StartsServiceImpl.java
@@ -15,7 +15,6 @@ import ru.practicum.ewm.repository.StatsRepository;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
