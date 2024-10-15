269a1f903eb6a35bbbd625bb8de8f5ef0912569f
ongonets <159029506+ongonets@users.noreply.github.com>
2024-10-15 18:46:11 +0300

    Add StatServer and StatClient (#4)
    * Add modules
    
    * Refactor docker-compose.yml
    
    * Add DTO
    
    * Add ParamDto
    
    * add stats-server
    
    * delete schema changes
    
    * fix checkstyle
    
    * Refactor dependency
    
    * Add StatClient
    
    * Add StatClientImpl
    
    * Fix codestyle
    
    * add stats-server
    
    * Refactor StatsController
    
    * Add client (#2)
    
    Add StatClient
    
    * Refactor StatClient
    
    * Fix problem with checkstyle
    
    ---------
    
    Co-authored-by: Elen-B <beskrovnaia.elena@yandex.ru>

---
+++ stats/stats-server/src/main/java/ru/practicum/ewm/service/StartsServiceImpl.java
@@ -0,0 +1,47 @@
+package ru.practicum.ewm.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.ewm.dto.HitDto;
+import ru.practicum.ewm.dto.StatDto;
+import ru.practicum.ewm.mapper.HitMapper;
+import ru.practicum.ewm.mapper.StatMapper;
+import ru.practicum.ewm.model.Hit;
+import ru.practicum.ewm.model.Stat;
+import ru.practicum.ewm.repository.StatsRepository;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StartsServiceImpl implements StatsService {
+    private final StatsRepository repository;
+    private final HitMapper hitMapper;
+    private final StatMapper statMapper;
+
+    @Override
+    @Transactional
+    public void addHit(HitDto paramHitDto) {
+        log.info("StartsServiceImpl/addHit args: {}", paramHitDto);
+        Hit hit = hitMapper.map(paramHitDto);
+        repository.save(hit);
+    }
+
+    @Override
+    public List<StatDto> getStats(String start, String end, List<String> uris, boolean unique) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime startTime = LocalDateTime.parse(start, formatter);
+        LocalDateTime endTime = LocalDateTime.parse(end, formatter);
+        List<Stat> result = repository.getStat(startTime, endTime, uris, unique);
+        return result.stream()
+                .map(statMapper::map)
+                .collect(Collectors.toList());
+    }
+}
