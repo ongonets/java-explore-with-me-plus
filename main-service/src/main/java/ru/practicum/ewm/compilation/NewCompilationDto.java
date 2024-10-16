cb3f2f4ad26d3228a2a88a98ee5ca95431e3e16d
iliam9 <ilkafa93@yandex.ru>
2024-10-16 23:23:15 +0300

    Model and Dto added

---
+++ main-service/src/main/java/ru/practicum/ewm/compilation/NewCompilationDto.java
@@ -0,0 +1,24 @@
+package ru.practicum.ewm.compilation;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewCompilationDto {
+
+    List<Long> events;
+
+    Boolean pinned;
+
+    @NotBlank
+    String title;
+
+}
