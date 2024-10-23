4083f9c23267d2a0a9f8dc2ee29a9d0ea1bac992
Vyacheslav Tetyuev <s1995t@yandex.ru>
2024-10-23 22:00:33 +0300

    fix deleting category with linked events

--- main-service/src/main/java/ru/practicum/ewm/event/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/event/repository/EventRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.ewm.event.model.Event;
 
 public interface EventRepository extends JpaRepository<Event,Long> {
+
+    boolean existsByCategoryId(long catId);
 }
